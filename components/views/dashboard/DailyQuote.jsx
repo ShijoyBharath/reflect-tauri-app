950d5639834858841054c5436bf4ff74e064ef39
Shijoy Bharath <shijoybharath8@gmail.com>
2024-04-05 16:46:49 +0530

    Added random daily quote

--- components/views/dashboard/DailyQuote.jsx
+++ components/views/dashboard/DailyQuote.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+"use client";
+import React, {useState, useEffect} from "react";
 import { Quote } from "lucide-react";
+import quotes from './quotes.json';
 
 const DailyQuote = () => {
+
+  const [quote, setQuote] = useState({quote : "", author: ""});
+
+  useEffect(() => {
+    // Select a random quote when the component mounts
+    const today = new Date();
+    // Calculate a unique index based on the current date
+    const index = Math.abs(today.getDate() * today.getMonth() * today.getFullYear()) % quotes.length;
+    // const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[index];
+    setQuote(randomQuote);
+  }, []);
+
   return (
     <div className="flex flex-col gap-4 m-4 p-9 bg-white rounded-lg">
       <Quote />
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        This too shall pass.
+        {quote["quote"]}
       </h1>
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-        - Unknown
+        - {quote["author"]}
       </h4>
     </div>
   );
