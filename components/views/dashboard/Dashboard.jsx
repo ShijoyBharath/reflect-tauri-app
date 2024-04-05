6c130366e69dde6b82a6bd7a4d95ae9fdd30519f
Shijoy Bharath <shijoybharath8@gmail.com>
2024-04-05 19:44:47 +0530

    updated dashboard to use sqlite data for population

--- components/views/dashboard/Dashboard.jsx
+++ components/views/dashboard/Dashboard.jsx
@@ -1,31 +1,31 @@
-import React from "react";
+"use client"
+import React, { useState, useEffect } from "react";
 import DailyQuote from "./DailyQuote";
 import StatCard from "./StatCard";
 import HabitsCard from "./HabitsCard";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import GoalsWidget from "./GoalsWidget";
+import Database from "tauri-plugin-sql-api";
 
 const Dashboard = () => {
-  const habits = [
-    {
-      id: 1,
-      calendar: "calone",
-      habit: "Gym",
-      description: "Do circuit training",
-    },
-    {
-      id: 2,
-      calendar: "caltwo",
-      habit: "Build Reflect",
-      description: "Complete capacitor.js app",
-    },
-    {
-      id: 3,
-      calendar: "calthree",
-      habit: "Do SEO for projects",
-      description: "Optimize",
-    },
-  ];
+
+  const [habits, setHabits] = useState([])
+
+  useEffect(()=>{
+    get_data().then((data)=> setHabits(data))
+  }, [])
+
+  async function get_data(start_date, end_date) {
+    try {
+      const db = await Database.load("sqlite:data.db");
+      const select = await db.select(
+        "SELECT * FROM habits",
+      );
+      return select;
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  }
 
   return (
     <div className="flex flex-col m-3 p-5 gap-3 justify-between bg-slate-200 rounded-lg">
@@ -46,7 +46,7 @@ const Dashboard = () => {
               key={item.id}
               habit={item.habit}
               description={item.description}
-              calendarId={item.calendar}
+              calendarId={"calendar-" + item.uuid}
             />
           );
         })}
