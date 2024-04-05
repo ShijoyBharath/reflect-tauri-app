5e114761a713e685a63efdeebe83c4a3c9c88613
Shijoy Bharath <shijoybharath8@gmail.com>
2024-04-05 14:37:04 +0530

    Daily goals update

--- components/views/goals/DailyGoals.jsx
+++ components/views/goals/DailyGoals.jsx
@@ -1,33 +1,70 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
 import Database from "tauri-plugin-sql-api";
 
 const DailyGoals = () => {
-
-  async function insert() {
-    try {
-      const db = await Database.load('sqlite:data.db');
-      const result = await db.execute("CREATE TABLE IF NOT EXISTS dailygoals (id INTEGER PRIMARY KEY, goal TEXT NOT NULL, date TEXT NOT NULL)");
-      const insert = await db.execute("INSERT INTO dailygoals (goal, date) VALUES (?, ?)", ["make something", "2024-04-04"])
-    } catch (error) {
-      console.log("error : ", error)
-    }
-  }
-  // insert();
-
   const [dailygoals, setDailyGoals] = useState({});
+  var [days, setDays] = useState({});
+  var [dbGoals, setDBGoals] = useState([]);
+  const today = formatDate(new Date());
+
+  useEffect(() => {
+    init_table();
+  }, []);
+
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setDailyGoals((prevPlans) => ({
       ...prevPlans,
-      [id]: value,
+      [id]: {
+        goal: value,
+        date: days[id][0],
+      },
     }));
   };
 
-  function getNextWeekDays(startDate) {
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  const getCurrentWeek = (startWeekDate) => {
+    const today = new Date();
+    for (
+      var d = new Date(startWeekDate);
+      d <= today;
+      d.setDate(d.getDate() + 7)
+    ) {
+      var temp_date = new Date(d);
+      var week_start = new Date(d);
+      var week_end = new Date(temp_date.setDate(temp_date.getDate() + 6));
+
+      if (today >= week_start && today <= week_end) {
+        return [formatDate(week_start), formatDate(week_end)];
+      }
+    }
+  };
+
+  const getCurrent12Weeks = (startDate) => {
+    const today = new Date();
+    for (var d = new Date(startDate); d <= today; d.setDate(d.getDate() + 84)) {
+      var temp_date = new Date(d);
+      var week_start = new Date(d);
+      var week_end = new Date(temp_date.setDate(temp_date.getDate() + 83));
+
+      if (today >= week_start && today <= week_end) {
+        return [formatDate(week_start), formatDate(week_end)];
+      }
+    }
+  };
+
+  const getThisWeekDates = (startDate) => {
     const daysOfWeek = [
       "Sunday",
       "Monday",
@@ -37,7 +74,7 @@ const DailyGoals = () => {
       "Friday",
       "Saturday",
     ];
-    const nextWeekDays = [];
+    const thisWeekDates = {};
 
     // Clone the start date to avoid mutating it
     const currentDate = new Date(startDate);
@@ -45,51 +82,120 @@ const DailyGoals = () => {
     // Iterate for the next 7 days
     for (let i = 0; i < 7; i++) {
       const day = daysOfWeek[currentDate.getDay()];
-      const date = currentDate.getDate();
-      const month = currentDate.getMonth() + 1; // Month is zero-based, so we add 1
+      const date = String(currentDate.getDate()).padStart(2, "0");
+      const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Month is zero-based, so we add 1
       const year = currentDate.getFullYear();
 
-      nextWeekDays.push([day, `${year}-${month}-${date}`]);
+      thisWeekDates[day] = [`${year}-${month}-${date}`, ""];
+      dbGoals.forEach((item) => {
+        if (item.date === `${year}-${month}-${date}`) {
+          thisWeekDates[day] = [`${year}-${month}-${date}`, item.goal];
+        }
+      });
 
       // Increment the date for the next day
       currentDate.setDate(currentDate.getDate() + 1);
     }
 
-    const today = new Date();
+    return thisWeekDates;
+  };
 
-    return [nextWeekDays, daysOfWeek[today.getDay()]];
+  const saveDailygoals = () => {
+    var dataArray = Object.keys(dailygoals).map((key) => dailygoals[key]);
+    dataArray.forEach((item) => {
+      insert_data(item.goal, item.date);
+    });
+    console.log("saved daily data");
+  };
+
+  async function init_table() {
+    try {
+      const db = await Database.load("sqlite:data.db");
+      const result = await db.execute(
+        "CREATE TABLE IF NOT EXISTS dailygoals (id INTEGER PRIMARY KEY, goal TEXT NOT NULL, date TEXT NOT NULL)"
+      );
+      const start_date = await db.select("SELECT * FROM appconfig");
+
+      var weekdates = getCurrent12Weeks(start_date[0].start_date);
+      var thisweek = getCurrentWeek(weekdates[0]);
+      get_data(thisweek[0], thisweek[1]).then((data) => setDBGoals(data));
+
+      var data = getThisWeekDates(thisweek[0], thisweek[1]);
+      setDays(data);
+    } catch (error) {
+      console.log("error : ", error);
+    }
   }
 
-  // Example usage:
-  const startDate = new Date(); // Use today's date as the start date
-  const [nextWeekDays, today] = getNextWeekDays(startDate);
-  // console.log(nextWeekDays);
-  // console.log(dailygoals);
+  async function insert_data(goal, date) {
+    try {
+      const db = await Database.load("sqlite:data.db");
+      const select = await db.select("SELECT * FROM dailygoals WHERE date=?", [
+        date,
+      ]);
+      if (select.length === 0) {
+        const insert = await db.execute(
+          "INSERT INTO dailygoals (goal, date) VALUES (?, ?)",
+          [goal, date]
+        );
+      } else {
+        const update = await db.execute(
+          "UPDATE dailygoals SET goal=? WHERE date=?",
+          [goal, date]
+        );
+      }
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  }
 
+  async function get_data(start_date, end_date) {
+    try {
+      const db = await Database.load("sqlite:data.db");
+      const select = await db.select(
+        "SELECT * FROM dailygoals WHERE DATE(date) BETWEEN DATE(?) AND DATE(?)",
+        [start_date, end_date]
+      );
+      return select;
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  }
 
   return (
     <div>
       <div className="flex flex-col gap-5 w-[500px] bg-slate-100 p-5 rounded-lg">
         <div className="flex justify-between gap-3">
           <h2>Daily Goals for Week 1</h2>
-          <h3>{"path"}</h3>
+          <Button onClick={() => saveDailygoals()}>Save</Button>
         </div>
         <div>
           <h6>These are your most impactful things you need to get done.</h6>
         </div>
         <div className="flex flex-col gap-4 p-4 pt-8">
-          {nextWeekDays.map((day) => (
+          {Object.keys(days).map((day) => (
             <div
-              className="grid w-full max-w-sm items-center gap-1.5"
-              key={day[0]}
+              key={day}
+              className={`p-2 rounded-lg ${
+                days[day][0] == today ? "bg-cyan-500" : ""
+              }`}
             >
-              <Label htmlFor={day[0]}>{day[0]}</Label>
-              <Input
-                type="text"
-                id={day[0]}
-                placeholder={"Plans for " + day[0] + " " + day[1]}
-                onChange={handleInputChange}
-              />
+              <div
+                className="grid w-full max-w-sm items-center gap-1.5"
+                key={day}
+              >
+                <Label htmlFor={day}>{day}</Label>
+                <Input
+                  type="text"
+                  id={day}
+                  placeholder={
+                    days[day][1]
+                      ? days[day][1]
+                      : "Plans for " + day + " " + days[day][0]
+                  }
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
           ))}
         </div>
