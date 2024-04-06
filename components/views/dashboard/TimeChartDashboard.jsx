0dfdd3f6791ad9c3066adc3aab7a016b9ce9dbc9
Shijoy Bharath <shijoybharath8@gmail.com>
2024-04-06 09:25:40 +0530

    Time chart dashboard fix for errors

--- components/views/dashboard/TimeChartDashboard.jsx
+++ components/views/dashboard/TimeChartDashboard.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { Component, useEffect, useState } from "react";
-// import Chart from "react-apexcharts";
 import dynamic from 'next/dynamic'
 import Database from "tauri-plugin-sql-api";
 
@@ -10,9 +9,7 @@ const TimeChartDashboard = () => {
   
 
   const [series, setSeries] = useState([])
-  const [categories, setCategories] = useState([])
-
-  const options = {
+  const [options, setOptions] = useState({
     chart: {
       id: "area-chart",
       toolbar: {
@@ -23,7 +20,7 @@ const TimeChartDashboard = () => {
       enabled: false,
     },
     xaxis: {
-      categories: categories,
+      categories: ["0"],
       axisTicks: {
         show: false,
       },
@@ -40,39 +37,47 @@ const TimeChartDashboard = () => {
     grid: {
       show: false,
     },
-  };
-
-  function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
-  }
+  })
   
   useEffect(()=>{
     get_data().then((data)=>{
       var ser = data.map((item)=>item.value)
       var cat = data.map((item)=>item.date)
-      setCategories(cat)
       setSeries([{
         name : "series-1",
         data : ser
       }])
+      setOptions({
+        chart: {
+          id: "area-chart",
+          toolbar: {
+            show: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        xaxis: {
+          categories: cat,
+          axisTicks: {
+            show: false,
+          },
+          axisBorder: {
+            show: false,
+          },
+        },
+        stroke: {
+          curve: "smooth",
+        },
+        fill: {
+          type: "gradient",
+        },
+        grid: {
+          show: false,
+        },
+      })
     })
-  })
-  
-  const getCurrent12Weeks = (startDate) => {
-    const today = new Date();
-    for (var d = new Date(startDate); d <= today; d.setDate(d.getDate() + 84)) {
-      var temp_date = new Date(d);
-      var week_start = new Date(d);
-      var week_end = new Date(temp_date.setDate(temp_date.getDate() + 83));
-
-      if (today >= week_start && today <= week_end) {
-        return [formatDate(week_start), formatDate(week_end)];
-      }
-    }
-  };
+  }, [])
 
   async function get_data() {
     try {
