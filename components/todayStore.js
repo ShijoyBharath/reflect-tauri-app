2bc6518e2e013d3413e65d4b973676ab8e5a5b3f
Shijoy Bharath <shijoybharath8@gmail.com>
2024-04-10 11:16:15 +0530

    Final release-ready

--- components/todayStore.js
+++ components/todayStore.js
@@ -1,8 +1,8 @@
 import {create} from 'zustand';
 
 const useTodayStore = create((set) => ({
-  // today: new Date,
-  todayGlobal: new Date("2024-04-16"),
+  todayGlobal: new Date(),
+  // todayGlobal: new Date("2024-04-16"),
   setTodayGlobal: (todayGlobal) => set({ todayGlobal }),
 }));
 
