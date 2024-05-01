f84febd26001ed957e52f3fd64ca6405a5195950
Shijoy Bharath <shijoybharath8@gmail.com>
2024-05-01 13:50:12 +0530

    theme, timer data in localstorage

--- app/page.js
+++ app/page.js
@@ -13,30 +13,32 @@ export default function Home() {
   const { refreshDashboard, setRefreshDashboard } = useDashboardStore();
 
   useEffect(() => {
-    check_activation();
+    // check_activation();
     setHelper(localStorage.getItem("helper"));
   }, [refreshDashboard]);
 
-  async function check_activation() {
-    try {
-      const db = await Database.load("sqlite:data.db");
-      const activated_data = await db.select("SELECT activated from appconfig");
-      setActivated(activated_data[0].activated);
-    } catch (error) {
-      console.log("error : ", error);
-    }
-  }
+  // async function check_activation() {
+  //   try {
+  //     const db = await Database.load("sqlite:data.db");
+  //     const activated_data = await db.select("SELECT activated from appconfig");
+  //     setActivated(activated_data[0].activated);
+  //   } catch (error) {
+  //     console.log("error : ", error);
+  //   }
+  // }
 
   return (
     <div>
-      {activated === 0 ? (
+      {/* {activated === 0 ? (
         <Dialog open={true}>
           <DialogTrigger asChild></DialogTrigger>
           <DialogContent>
             <ActivateFromTrial />
           </DialogContent>
         </Dialog>
-      ) : helper == 0 ? (
+      ) :  */}
+      {
+      helper == 0 ? (
         <Dialog open={true}>
           <DialogTrigger asChild></DialogTrigger>
           <DialogContent>
